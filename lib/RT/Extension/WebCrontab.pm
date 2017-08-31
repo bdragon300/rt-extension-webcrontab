87160057da40f5d3e888a7900910227855a7b582
Igor Derkach <gosha753951@gmail.com>
2017-08-31 22:32:21 +0300

    Much refactoring

--- lib/RT/Extension/WebCrontab.pm
+++ lib/RT/Extension/WebCrontab.pm
@@ -99,6 +99,24 @@ my %module_list = (
         ResolveReminder !]
 );
 
+=head2 get_available_values() -> \%values
+
+Returns available values for each rt-crontool parameter
+
+Returns:
+
+HASHREF with following data:
+
+    {
+    search => \@search_classes,
+    condition => \@condition_classes,
+    action => \@action_classes,
+    template => \@template_names,
+    transaction => \@transaction,
+    transaction-type => \@transaction_types
+    }
+
+=cut
 
 sub get_available_values {
     my $vals = clone(\%module_list);
@@ -120,31 +138,46 @@ sub get_available_values {
     return $vals;
 }
 
+
+=head2 get_available_env_vars() -> \@available_vars
+
+Returns available environmental variables
+
+=cut
+
 sub get_available_env_vars {
     return clone(\@available_env_vars);
 }
+
+
+=head2 get_classes() -> \@classes
+
+Returns rt-crontool available parameters
+
+=cut
 
 sub get_classes {
     return clone(\@classes);
 }
 
-sub load_events {
-    my $e = load_crontab();
-    return $e->{'events'};
-}
 
-sub load_env {
-    my $e = load_crontab();
-    return $e->{'env'};
-}
+=head2 load_crontab() -> \%crontab_structure
+
+Loads current user crontab and returns its contents in structured view
+
+Returns:
+
+HASHREF with following data:
+
+    {
+    'events' => events array,
+    'env' => environment variables hash,
+    'dump' => crontab dump as string
+    }
+
+=cut
 
 sub load_crontab {
-    my $crontab = {
-        'events' => [],
-        'env'    => {},
-        'dump' => []
-    };
-
     my $ct = new Config::Crontab;
     $ct->read || RT::Logger->warning("[RT::Extension::WebCrontab]: User crontab does not exist and will be created");
     if ($ct->error) {
@@ -152,91 +185,244 @@ sub load_crontab {
         $ct->error('');
         return (undef);
     }
+    my %crontab = (
+        'events' => _read_events($ct),
+        'env'    => _read_env($ct),
+        'dump'   => $ct->dump
+    );
 
-    foreach my $event ($ct->select( -type => 'event' )) {
-        my $e = {};
+    return \%crontab;
+}
+
+
+=head2 save_events(\@events) -> (success, error_msg)
+
+Save given events
+
+Parameters:
+
+=over
+
+=item events - ARRAYREF to events structure
+
+=back
+
+=cut
+
+sub save_events {
+    my $e = shift;
+    save_crontab(load_crontab(), events => $e);
+}
+
+
+=head2 save_env(\%env_vars) -> (success, error_msg)
+
+Save given environmental variables
+
+Parameters:
+
+=over
+
+=item env_vars - HASHREF with variables
+
+=back
+
+=cut
+
+sub save_env {
+    my $e = shift;
+    save_crontab(load_crontab(), env => $e);
+}
+
+
+=head2 save_crontab(old_crontab, \@events=>undef, \%env=>undef) -> (success, error_msg)
+
+Saves crontab
+
+Parameters:
+
+=over
+
+=item old_crontab - crontab structure, see load_crontab() docs
+
+=item events - ARRAYREF, Optional. Events structure to be save. If undef the old ones will be used
+
+=item env - HASHREF, Optional. Env vars to be save. If undef the old ones will be used
+
+=back
+
+=cut
+
+sub save_crontab {
+    my $old_crontab = shift;
+    my %args = (
+        events => undef, # type: ARRAYREF
+        env => undef,    # type: HASHREF
+        @_
+    );
+
+    my $events = _build_events($args{events} // $old_crontab->{'events'} // []);
+
+    my %vars = %{ $args{env} // $old_crontab->{'env'} // {} };
+    my %vars_ok = %vars{grep exists($vars{$_}), @available_env_vars}; # only available vars
+    my $env = _build_env(\%vars_ok);
+
+    my $cb = new Config::Crontab::Block( -lines => $events );
+    $cb->first(@$env);
+    my $ct = new Config::Crontab;
+    $ct->blocks([$cb]);
+
+    #print $ct->data;
+    $ct->error('');
+    return ($ct->write, $ct->error);
+}
+
+
+=head2 _read_events(crontab) -> \@events
+
+Extract events from crontab object
+
+Parameters:
+
+=over
+
+=item crontab - Config::Crontab object
+
+=back
+
+Returns:
+
+ARRAYREF to event hash array:
+
+    {
+    'skip' => 0,  # 1 when not rt-crontool command
+    'obj' => Config::Crontab::Event object,
+    'expression' => crontab expression,
+    arg => value, # rt-crontool parameters, see @classes package var
+    ...
+    }
+
+=cut
+
+sub _read_events {
+    my $crontab = shift;
+    my @events = ();
+
+    foreach my $event ($crontab->select( -type => 'event' )) {
+        my %e = (
+            'skip' => 0,
+            'obj' => undef,
+            'expression' => undef
+        );
         my $cmd = $event->command;
         utf8::decode($cmd) unless utf8::is_utf8($cmd);
         my @shwords = shellwords(($cmd));
 
+        # Mark as 'skip' non rt-crontool command
         if ($shwords[0] !~ /.*rt-crontool$/) {
-            $RT::Logger->warning("[RT::Extension::WebCrontab]: Skip event: " . $cmd);
+            $RT::Logger->warning("[RT::Extension::WebCrontab]: Skip event: " . $cmd);  # FIXME: reduce log lvl
 
-            $e->{'skip'} = 1;
-            $e->{'obj'} = $event;
-            push @{$crontab->{'events'}}, clone($e);
+            $e{'skip'} = 1;
+            $e{'obj'} = $event;
+            push @events, clone(\%e);
 
             next;
         } else {
-            shift @shwords;
+            shift @shwords; #FIXME: do not print commented strings
         }
         
-        $e->{'expression'} = $event->datetime;
+        $e{'expression'} = $event->datetime;
         while (@shwords) {
             my $shword = shift @shwords;
 
             if (my @c = grep { $shword eq ('--' . $_) } @classes) {
                 my $class = $c[0];
 
-                $e->{$class} = '';
+                $e{$class} = '';
                 next if (exists($shwords[0]) && $shwords[0] =~ /^--/); # no value for current parameter
 
                 my $v = shift @shwords;
                 $v =~ s/^"(.*)"$/$1/g; # unquote
                 $v =~ s/\\([\\"])/$1/g; # unescape
 
-                $e->{$class} = $v;
+                $e{$class} = $v;
             }
         }
 
-        push @{$crontab->{'events'}}, clone($e);
+        push @events, clone(\%e);
     }
 
-    foreach my $env ($ct->select( -type => 'env' )) {
+    return \@events;
+}
+
+
+=head2 _read_env(crontab) -> \%vars
+
+Extract environment variables from crontab object
+
+Parameters:
+
+=over
+
+=item crontab - Config::Crontab object
+
+=back
+
+Returns:
+
+HASHREF with variables value
+
+=cut
+
+sub _read_env {
+    my $crontab = shift;
+    my %vars = ();
+
+    foreach my $env ($crontab->select( -type => 'env' )) {
         my $v = $env->value;
         utf8::decode($v) unless utf8::is_utf8($v);
         $v =~ s/^"(.*)"$/$1/g; # unquote
         $v =~ s/\\([\\"])/$1/g; # unescape
 
-        $crontab->{'env'}->{$env->name} = $v;
+        $vars{$env->name} = $v;
     }
-    $crontab->{'dump'} = $ct->dump;
 
-    return $crontab;
+    return \%vars;
 }
 
-sub save_events {
-    my $e = shift;
-    save_crontab({'events' => $e});
-}
 
-sub save_env {
-    my $e = shift;
-    save_crontab({'env' => $e});
-}
+=head2 _build_events(\@events) -> \@event_objects
 
-sub save_crontab {
-    my $crontab = shift;
-    my @events;
-    my @env;
-    my $old_crontab = load_crontab();
-    
-    # Events
-    ## rt-crontool command
-    my $cmd_path = $RT::BinPath;
-    $cmd_path =~ s/([^\/])$/$1\// if $cmd_path; #Add trailing slash
-    my $cmd_exe = $cmd_path . 'rt-crontool';
+Builds Config::Crontab::Event objects from events structure. See _read_events docs
 
-    $crontab->{'events'} = $old_crontab->{'events'} unless (exists($crontab->{'events'}));
+Parameters:
 
-    for my $event (@{$crontab->{'events'}}) {
+=over
 
+=item events - ARRAYREF to events structures
+
+=back
+
+Returns:
+
+Config::Crontab::Event array
+
+=cut
+
+sub _build_events {
+    my $events = shift;
+
+    my $cmd_exe = _get_crontool_path();
+    my @res = ();
+
+    for my $event (@$events) {
         my @cmd_params = ();
-        # Non-rt-crontool call
+
+        # Write non rt-crontool call in its original form
         if ($event->{'skip'}
-            && ref($event->{'obj'}))
+            && ref($event->{'obj'})) # FIXME: split skip and ref checks
         {
-            push @events, $event->{'obj'};
+            push @res, $event->{'obj'};
             next;
         }
 
@@ -262,31 +448,62 @@ sub save_crontab {
 
         my $cmd = join(' ', ($cmd_exe, @cmd_params));
         my $expression = $event->{'expression'};
-        push @events, new Config::Crontab::Event( -datetime => $expression, -command => $cmd );
+        push @res, new Config::Crontab::Event( -datetime => $expression, -command => $cmd );
     }
 
-    # Environment variables
-    $crontab->{'env'} = $old_crontab->{'env'} unless (exists($crontab->{'env'}));
-    for my $e (@available_env_vars) {
-        next unless exists($crontab->{'env'}->{$e});
+    return \@res;
+}
 
+
+=head2 _build_env(\%variables) -> \@env_objects
+
+Builds Config::Crontab::Env objects from variables hash
+
+Parameters:
+
+=over
+
+=item variables - HASHREF with variables values
+
+=back
+
+Returns:
+
+Config::Crontab::Env array
+
+=cut
+
+sub _build_env {
+    my $vars = shift;
+    my @env = ();
+
+    for my $k (keys %$vars) {
+        my $v = $vars->{$k};
         # escape and quote
-        my $v = $crontab->{'env'}->{$e};
         $v =~ s/(["\\])/\\$1/g;
         $v = '"' . $v . '"';
 
-        push @env, new Config::Crontab::Env( -name => $e, -value => $v );
+        push @env, new Config::Crontab::Env( -name => $k, -value => $v );
     }
 
+    return \@env;
+}
 
-    my $cb = new Config::Crontab::Block( -lines => \@events );
-    $cb->first(@env);
-    my $ct = new Config::Crontab;
-    $ct->blocks([$cb]);
 
-    #print $ct->data;
-    $ct->error('');
-    return ($ct->write, $ct->error);
+=head2 _get_crontool_path() -> rt-crontool_path
+
+Returns absolute path to the rt-crontool command
+
+Returns:
+
+"/path/to/the/rt-crontool"
+
+=cut
+
+sub _get_crontool_path {
+    my $cmd_path = $RT::BinPath;
+    $cmd_path =~ s/([^\/])$/$1\// if $cmd_path; # Add trailing slash if needed
+    return $cmd_path . 'rt-crontool';
 }
 
 1;
